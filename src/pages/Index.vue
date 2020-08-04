3d0f552971658084ed3e9bf5b1140b4461365a64
Nam Nguyen <nam.nguyen.node@gmail.com>
2020-08-04 14:19:30 +0700

    Static rock

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,18 +1,50 @@
 <template>
   <q-page class="flex column">
     <div class="col q-pt-lg q-mx-md">
-      <q-input v-model="text" :dense="dense" label="Label" counter bottom-slots maxlength="12">
+      <q-input
+        v-model="search"
+        dark
+        placeholder="Search"
+        borderless
+      >
         <template v-slot:before>
-          <q-icon name="event"></q-icon>
+          <q-icon name="my_location"></q-icon>
         </template>
 
         <template v-slot:hint>Field hint</template>
 
         <template v-slot:append>
-          <q-btn round dense flat icon="add"></q-btn>
+          <q-btn
+            round
+            dense
+            flat
+            icon="search"
+          ></q-btn>
         </template>
       </q-input>
     </div>
+
+    <div class="col text-white text-center">
+      <div class="text-h4 text-weight-light">
+        Manchester
+      </div>
+      <div class="text-h6 text-weight-light">
+        Rain
+      </div>
+      <div class="text-h1 text-weight-thin q-my-lg relative-position">
+        <span>8</span>
+        <span class="text-h4 relative-position degree">&deg;</span>
+      </div>
+    </div>
+
+    <div class="col text-center">
+      <img
+        src="https://www.fillmurray.com/100/100"
+        alt="Bill"
+      >
+    </div>
+
+    <div class="col skyline"></div>
   </q-page>
 </template>
 
@@ -21,11 +53,27 @@ import { defineComponent } from '@vue/composition-api';
 
 export default defineComponent({
   name: 'PageIndex',
+  data() {
+    return {
+      search: '',
+    };
+  },
 });
 </script>
 
 <style lang="scss">
 .q-page {
   background: linear-gradient(to bottom, #136a8a, #267871);
+}
+
+.degree {
+  top: -44px;
+}
+
+.skyline {
+  flex: 0 0 100px;
+  background: url('../static/skyline.png');
+  background-size: contain;
+  background-position: center bottom;
 }
 </style>
